8cf95a4a81fa83f36bba821737b231bfdcb1a461
getin6657 <getin6657@hotmail.com>
2023-01-24 14:38:06 -0300

    Corrigindo problems login.js

--- login.js
+++ login.js
@@ -2,17 +2,14 @@
 
 //Finalizando sistema de login
 
-<<<<<<< HEAD
+
 function login (nome, email){
     return 'Bem vindo';
 }
 
-
 // Seja bem vindo
-=======
 
 // Teste branch
 
 // Arrumando sistema de login
 // do curso de git
->>>>>>> login
